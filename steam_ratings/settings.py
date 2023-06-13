9af8f9ecbc98f56026b37e1357242ccc62d1dbe1
Nobel Yoo <nobr@nobr.me>
2023-06-13 05:59:05 +0000

    remove and rotate sensitive info

--- steam_ratings/settings.py
+++ steam_ratings/settings.py
@@ -1,11 +1,12 @@
+import os
+
 DEBUG = False
 TESTING = False
 CSRF_ENABLED = True
-SECRET_KEY = '25f94dba-91d7-4355-87c8-1581ec245034'
 
 POSTGRES = {
     'user': 'steam_ratings',
-    'pw': '85aed77c-49a2-40e0-b86e-1611f167afb3',
+    'pw': os.environ['DB_PW'],
     'db': 'steam_ratings',
     'host': '10.180.184.6'
 }
